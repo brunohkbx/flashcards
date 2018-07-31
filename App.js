93968e4968440cd6f355668342d5ab8da6d3fb2a
Bruno Castro <bruno.f.castro12@hotmail.com>
2018-07-31 09:01:36 -0300

    feat: Create Main screen to show decks

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import Main from './app/screens/Main';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <PaperProvider>
+        <Main />
+      </PaperProvider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
