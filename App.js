af2fef1adc48e017b24767fdf930f6271eb347ee
Bruno Castro <bruno.f.castro12@hotmail.com>
2018-07-31 22:09:04 -0300

    chore: Setup redux

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
+import { Provider as StoreProvider } from 'react-redux';
 import Main from './app/screens/Main';
+import store from './app/config/store';
 
 export default class App extends React.Component {
   render() {
     return (
-      <PaperProvider>
-        <Main />
-      </PaperProvider>
+      <StoreProvider store={store}>
+        <PaperProvider>
+          <Main />
+        </PaperProvider>
+      </StoreProvider>
     );
   }
 }
