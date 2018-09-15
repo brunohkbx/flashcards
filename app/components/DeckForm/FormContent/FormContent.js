23d04e252d6d7a955b434d8a7c7be5ea51dd8480
Bruno Castro <bruno.f.castro12@hotmail.com>
2018-09-15 12:30:22 -0300

    feat: Add fade animation when flashcard is removed on CreateDeck screen

--- app/components/DeckForm/FormContent/FormContent.js
+++ app/components/DeckForm/FormContent/FormContent.js
@@ -1,57 +1,78 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { ScrollView } from 'react-native';
 import { Headline } from 'react-native-paper';
 import { Field } from 'formik';
 import FormInput from '../FormInput/FormInput';
 import Flashcard from '../../Flashcard';
+import { Fadable } from '../../Animations';
 
-const FormContent = props => {
-  const {
-    handleChange,
-    values,
-    scrollViewRef,
-    onFlashcardDeleted
-  } = props;
+class FormContent extends Component {
+  state = {
+    flashcardToRemove: null
+  }
 
-  return (
-    <ScrollView
-      ref={scrollViewRef}
-    >
-      <Field
-        name="title"
-        label="Title"
-        component={FormInput}
-        onChangeText={handleChange('title')}
-        autoFocus
-      />
-      <Headline>Cards</Headline>
-      {
-        values.questions.map((question, index) => (
-          <Flashcard 
-            key={question.id} 
-            onFlashcardDeleted={() => onFlashcardDeleted(question.id)}
-          >
-            <Field
-              name={`questions.${index}.question`}
-              label="Question"
-              component={FormInput}
-              onChangeText={handleChange(`questions.${index}.question`)}
-              autoFocus
-              multiline
-            />
-            <Field
-              name={`questions.${index}.answer`}
-              label="Answer"
-              component={FormInput}
-              onChangeText={handleChange(`questions.${index}.answer`)}
-              multiline
-            />
-          </Flashcard>
-        ))
-      }
-    </ScrollView>
-  );
+  handleFlashcardDelete = id => {
+    const { onFlashcardDeleted } = this.props;
+
+    this.setState({ flashcardToRemove: null }, () => onFlashcardDeleted(id));
+  };
+
+  render() {
+    const {
+      handleChange,
+      values,
+      scrollViewRef
+    } = this.props;
+
+    const { flashcardToRemove } = this.state;
+
+    return (
+      <ScrollView
+        ref={scrollViewRef}
+      >
+        <Field
+          name="title"
+          label="Title"
+          component={FormInput}
+          onChangeText={handleChange('title')}
+          autoFocus
+        />
+        <Headline>Cards</Headline>
+        {
+          values.questions.map((question, index) => (
+            <Fadable
+              key={question.id}
+              fade={flashcardToRemove === question.id}
+              onAnimationEnd={() => this.handleFlashcardDelete(question.id)}
+            >
+              <Flashcard
+                onFlashcardDeleted={
+                  () => this.setState({ flashcardToRemove: question.id })
+                }
+              >
+                <Field
+                  name={`questions.${index}.question`}
+                  label="Question"
+                  component={FormInput}
+                  onChangeText={handleChange(`questions.${index}.question`)}
+                  autoFocus
+                  multiline
+                />
+                <Field
+                  name={`questions.${index}.answer`}
+                  label="Answer"
+                  component={FormInput}
+                  onChangeText={handleChange(`questions.${index}.answer`)}
+                  multiline
+                />
+              </Flashcard>
+            </Fadable>
+          ))
+        }
+      </ScrollView>
+    );
+  }
 };
 
 FormContent.propTypes = {
