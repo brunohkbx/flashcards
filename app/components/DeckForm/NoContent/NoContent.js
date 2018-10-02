7e3901cf40dc797352a8c85940cd4369bd5252e5
Bruno Castro <bruno.f.castro12@hotmail.com>
2018-10-02 17:23:40 -0300

    feat: Show NoContent component on DeckForm when values are empty

---
+++ app/components/DeckForm/NoContent/NoContent.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Image } from 'react-native';
+import { Caption, Colors } from 'react-native-paper';
+import CenteredContainer from '../../../components/CenteredContainer';
+import Avatar from '../../../components/Avatar';
+
+const NoContent = () => {
+  return (
+    <CenteredContainer>
+      <Avatar
+        width={100}
+        height={100}
+        borderRadius={50}
+        backgroundColor={Colors.white}
+      >
+        <Image
+          source={require('../../../assets/images/flashcard.png')}
+          style={{ width: 64, height: 64 }}
+        />
+      </Avatar>
+      <Caption style={{ fontSize: 18}}>No flashcards</Caption>
+    </CenteredContainer>
+  );
+};
+
+export default NoContent;
